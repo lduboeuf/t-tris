b2bae7b4d0b41bdd33ae6a2e620aa64bcf51f4f5
Lionel Duboeuf <lduboeuf@ouvaton.org>
2019-03-27 10:08:24 +0100

    clean api

--- backend/api.php
+++ backend/api.php
@@ -1,24 +1,20 @@
-
 <?php
-
 /**quick and dirty apis**/
+
 include_once("config.inc.php");
 
-// foreach ($_SERVER as $key => $value) {
-//     echo $key."=>".$value;
-// }
-
 $token = filter_input(INPUT_SERVER, 'HTTP_TOKEN');
-//echo $token."==".TOKEN;
-if ($token!=TOKEN) header("HTTP/1.0 403 Forbidden");
+
+if ($token!=TOKEN){
+    header("HTTP/1.0 403 Forbidden");
+    exit;
+}
 
 
 $db = mysqli_connect(SQL_HOST, SQL_USERNAME, SQL_PASSWORD, SQL_DBNAME);
 
 if (!$db) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    header("HTTP/1.1 500 Internal Server Error");
     exit;
 }
 
@@ -43,10 +39,11 @@ if ($request_method=='GET'){
 
 }elseif ($request_method=='POST'){
 
-    $contentType = filter_input(INPUT_SERVER, 'Content-Type');
-    echo $contentType;
+    $contentType = filter_input(INPUT_SERVER, 'CONTENT_TYPE');
+    
     if(strcasecmp($contentType, 'application/json') != 0){
         header("HTTP/1.0 400 Bad Request");
+        exit;
     }
     
     //Receive the RAW post data.
@@ -59,6 +56,7 @@ if ($request_method=='GET'){
     //If json_decode failed, the JSON is invalid.
     if($decoded==null){
         header("HTTP/1.0 400 Bad Request");
+        exit;
     }
 
 
@@ -79,7 +77,7 @@ function can_save($current_score){
     //echo "kikou2:".$current_score;
 
     $stmt = mysqli_prepare($db,"SELECT count(*) as nb FROM score WHERE score > ?");
-    $stmt->bind_param("s", $current_score);
+    $stmt->bind_param("i", $current_score);
     $stmt->execute();
     $result  =  $stmt->get_result();
     $row = $result->fetch_array(MYSQLI_ASSOC);
@@ -117,7 +115,6 @@ function save_score($data){
     $canSave = can_save($data["score"]);
    
     if ($canSave == FALSE){
-        echo "canSave:".(int)$canSave;
         header("HTTP/1.1 208 Already Reported");
         return;
     } 
