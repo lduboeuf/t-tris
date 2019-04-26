e2b5deec2a6d2b0793a4683cc61e151ab763db29
Lionel Duboeuf <lduboeuf@ouvaton.org>
2019-04-26 10:49:55 +0200

    add backend home page

---
+++ backend/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>t-tris</title>
+</head>
+<body>
+
+<h1>Welcome to t-tris High Score</h1>
+
+<div id="high-scores">
+
+<ul>
+
+<?php
+
+include_once("config.inc.php");
+
+$db = mysqli_connect(SQL_HOST, SQL_USERNAME, SQL_PASSWORD, SQL_DBNAME);
+
+if (!$db) {
+    header("HTTP/1.1 500 Internal Server Error");
+    exit;
+}
+
+$query="SELECT * FROM score  ORDER BY score.score DESC LIMIT 100";
+$response=array();
+$result=mysqli_query($db, $query);
+while($row=mysqli_fetch_array($result, MYSQLI_ASSOC))
+{
+    echo "<li>".$row["name"]." ".$row["score"]."</li>";
+}
+
+mysqli_close($db);
+
+?>
+</ul>
+</div>
+    
+</body>
+</html>
